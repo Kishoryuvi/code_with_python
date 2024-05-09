3b798fbaeadb33f91ed7637edf7e3d0606c6c11c
Yuvraj Kishor <77017763+Kishoryuvi@users.noreply.github.com>
2024-05-09 14:25:51 +0530

    Create div_even_while_loop.py

---
+++ div_even_while_loop.py
@@ -0,0 +1,15 @@
+def div_even(n):
+    
+    i=1
+    while(i<n+1):
+        if(i%4!=0):
+            print(i)
+        i+=1
+        
+
+        
+            
+        
+print("Enter the number")
+n=int(input())
+div_even(n)        
